25edb2d3b92c1feeb7853dccde57c44b7137d171
Delique Allen <allen_delique@columbusstate.edu>
2025-11-03 08:57:31 -0500

    add csrf token to form component

--- resources/views/components/form.cp.php
+++ resources/views/components/form.cp.php
@@ -1,6 +1,7 @@
-<form 
+<form
     class="<?= htmlspecialchars($class ?? '') ?>"
     method="<?= htmlspecialchars($method ?? 'POST') ?>"
     action="<?= htmlspecialchars($action ?? '') ?>">
     <?= $slot ?? '' ?>
+    <input type="hidden" name="_csrf_token" value="<?= htmlspecialchars(generateCsrfToken()) ?>">
 </form>
