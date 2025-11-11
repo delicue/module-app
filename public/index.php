a1854458ea8ffaeb8306a1f9c23eb19541fda223
Delique Allen <allen_delique@columbusstate.edu>
2025-11-11 16:50:40 -0500

    remove unnecessary log

--- public/index.php
+++ public/index.php
@@ -5,10 +5,11 @@ use App\Module;
 
 require __DIR__ . '/../src/app/setup.php';
 
-Log::info('Received request');
 // Dispatch the request. This will populate any variables needed in the partials as well.
 $content = Module::getRouter('main')->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+
 Log::info("Dispatched {$_SERVER['REQUEST_METHOD']} request for {$_SERVER['REQUEST_URI']}");
+
 require view('partials/header');
 require $content;
 require view('partials/footer');
